9748b023ee067b3a81696dc994b99d31803de3f9
Bit0r <nie_wang@outlook.com>
2025-01-06 11:41:12 +0800

    feat: 更新下载脚本，添加 GoAccess 进行日志分析

--- deploy/download.fish
+++ deploy/download.fish
@@ -7,3 +7,14 @@ else
 end
 
 rsync --log-file=./logs/download.log -azvvvP $remote_host:/var/log/{caddy,pichat}/ ./logs/$remote_host/
+
+if [ -z $NUXT_SITE_NAME ]
+    set NUXT_SITE_NAME (basename $remote_path)
+end
+
+goaccess \
+    ./logs/$remote_host/$NUXT_SITE_NAME.jsonl \
+    -o ./logs/$remote_host/$NUXT_SITE_NAME.html \
+    -j 6 \
+    --datetime-format '%FT%T' \
+    --log-format '{"ts":"%x.%f%z","request":{"client_ip":"%h","proto":"%H","method":"%m","host":"%v","uri":"%U","headers":{"User-Agent":["%u"],"Referer":["%R"]},"tls":{"cipher_suite":"%k","proto": "%K"}},"duration": "%T","size": "%b","status": "%s","resp_headers":{"Content-Type":["%M"]}}'
